e4e0097e085ca9eaf09d852666c730af0f2f8a42
Jonathan Chasse <jonathan.r.chasse@gmail.com>
2020-07-11 14:53:22 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,8 +16,15 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
+    contact_details_hash.each do |attribute, data|
+      binding.pry
 
-    binding.pry
+    end
+
+
+
+    # if contact_details_hash[:favorite_ice_cream_flavors] = "strawberry"
 
 
   end
